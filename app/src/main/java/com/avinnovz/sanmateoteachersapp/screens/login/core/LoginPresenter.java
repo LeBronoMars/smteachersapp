27398a3aa3013887ec9c8dfc37b7a1be254df040
Russel Bulanon <rbulanon@exist.com>
2017-04-19 10:45:30 +0800

    Aligned codes in LoginPresenter

--- app/src/main/java/com/avinnovz/sanmateoteachersapp/screens/login/core/LoginPresenter.java
+++ app/src/main/java/com/avinnovz/sanmateoteachersapp/screens/login/core/LoginPresenter.java
@@ -2,6 +2,7 @@ package com.avinnovz.sanmateoteachersapp.screens.login.core;
 
 import com.avinnovz.sanmateoteachersapp.base.BasePresenter;
 import com.avinnovz.sanmateoteachersapp.data.api.ApiInterface;
+import com.avinnovz.sanmateoteachersapp.helpers.TokenManager;
 
 import javax.inject.Inject;
 
@@ -18,21 +19,21 @@ public class LoginPresenter extends BasePresenter implements LoginMvp.Presenter 
     LoginInteractor interactor;
 
     @Inject
-    public LoginPresenter(LoginMvp.View view, Retrofit retrofit, ApiInterface apiInterface){
+    public LoginPresenter(LoginMvp.View view, Retrofit retrofit, ApiInterface apiInterface) {
         //Attach the view to make it accessible on the BasePresenter
         attachView(view);
         this.view = view;
-        this.interactor = new LoginInteractor(this,retrofit,apiInterface);
+        this.interactor = new LoginInteractor(this, retrofit, apiInterface);
     }
 
     @Override
     public void onAuthenticateUser(String username, String password) {
-        if (username.isEmpty()){
+        if (username.isEmpty()) {
             view.setUsernameEmptyError();
-        }else if (password.isEmpty()){
+        } else if (password.isEmpty()) {
             view.setPasswordEmptyError();
-        }else {
-            interactor.onAuthenticateUser(username,password);
+        } else {
+            interactor.onAuthenticateUser(username, password);
         }
     }
 }
